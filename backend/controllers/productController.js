954c4115e501ce47c5d78db0f75c9553abe071c4
Paweł Hińcza <pawelsan@yahoo.com>
2022-05-02 14:54:05 +0200

    add error handling

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,34 +1,40 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc Get products
 // @route GET /api/products
 // @access Private
 
-const getProducts = (req, res) => {
+const getProducts = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get products" });
-};
+});
 
 // @desc Create product
 // @route POST /api/products
 // @access Private
 
-const createProduct = (req, res) => {
+const createProduct = asyncHandler(async (req, res) => {
+  if (!req.body.name) {
+    res.status(400);
+    throw Error("Please add a name field");
+  }
   res.status(200).json({ message: "Create products" });
-};
+});
 
 // @desc Update products
 // @route PUT /api/products/:id
 // @access Private
 
-const updateProduct = (req, res) => {
+const updateProduct = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update product ${req.params.id}` });
-};
+});
 
 // @desc Delete products
 // @route DELETE /api/products/:id
 // @access Private
 
-const deleteProduct = (req, res) => {
+const deleteProduct = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete product ${req.params.id}` });
-};
+});
 
 module.exports = {
   getProducts,
